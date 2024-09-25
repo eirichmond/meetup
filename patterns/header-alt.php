d5cb116988e137e557d88bb030068ba34a8abb7d
eirichmond <elliott@squareonemd.co.uk>
2024-09-25 14:06:20 +0100

    update post meetup

--- patterns/header-alt.php
+++ patterns/header-alt.php
@@ -5,6 +5,18 @@
  * Inserter: no
  */
 ?>
-<!-- wp:paragraph -->
-<p><?php esc_html_e('lksjdfsdf', 'meetup');?></p>
-<!-- /wp:paragraph -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"0"}},"position":{"type":""}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-black-background-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"base","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignwide has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"white","fontSize":"medium"} /-->
+
+<!-- wp:navigation {"textColor":"white","overlayBackgroundColor":"quaternary","overlayTextColor":"white","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","style":{"border":{"radius":"5px","width":"0px","style":"none"},"spacing":{"padding":{"left":"var:preset|spacing|30","right":"var:preset|spacing|30","top":"0","bottom":"0"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" style="border-style:none;border-width:0px;border-radius:5px;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><?php esc_html_e('Join us', 'meetup');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
--- patterns/header.php
+++ patterns/header.php
@@ -9,11 +9,17 @@
 <div class="wp-block-group alignfull"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-player-7792956_1920.jpg","dimRatio":0,"overlayColor":"black","isUserOverlayColor":true,"minHeight":50,"contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
 <div class="wp-block-cover alignfull" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-player-7792956_1920.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"var:preset|spacing|50"}},"color":{"gradient":"linear-gradient(135deg,rgba(0,0,0,0.62) 0%,rgba(0,0,0,0.62) 100%)"},"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide has-background" style="background:linear-gradient(135deg,rgba(0,0,0,0.62) 0%,rgba(0,0,0,0.62) 100%);margin-top:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"base","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
-<div class="wp-block-group alignwide has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0"><!-- wp:group {"layout":{"type":"flex"}} -->
-<div class="wp-block-group"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"white","fontSize":"medium"} /--></div>
+<div class="wp-block-group alignwide has-base-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"white","fontSize":"medium"} /-->
+
+<!-- wp:navigation {"textColor":"white","overlayBackgroundColor":"quaternary","overlayTextColor":"white","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:navigation {"textColor":"white","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} /--></div>
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","style":{"border":{"radius":"5px","width":"0px","style":"none"},"spacing":{"padding":{"left":"var:preset|spacing|30","right":"var:preset|spacing|30","top":"0","bottom":"0"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" style="border-style:none;border-width:0px;border-radius:5px;padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><?php esc_html_e('Join us', 'meetup');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
